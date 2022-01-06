4702878092460c2b8034a87fcce44594c5f9a847
mizan2832 <=>
2022-01-06 15:11:54 +0600

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{Component} from 'react';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Clock from './components/Clock';
+import HookCounter from './components/HookCounter';
+class App extends Component{
+    render(){
+        return(
+            <div>
+               <HookCounter />
+            </div>
+        );
+    }
 }
-
 export default App;
---
+++ src/components/Greet.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Greet = () => <h1> I have to keep my patience. </h1>;
+export default Greet;
